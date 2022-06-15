d02ece38664028428b5d3a9f4ff74985a84b02d1
Mohammad H Alijany <mh.alijany@gmail.com>
2022-06-15 16:38:46 +0430

    reset :|

--- main.go
+++ main.go
@@ -1,30 +1,22 @@
 package main
 
 import (
+	"log"
 	"main/common"
-	"net/http"
+	"main/devices"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	switch req.HTTPMethod {
-	case "GET":
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusOK,
-			Body:       string(`{ "hello": "mamad" }`),
-		}, nil
-	case "POST":
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusOK,
-			Body:       string(`{ "hello": "mamad" }`),
-		}, nil
-	default:
-		return common.ClientError(http.StatusMethodNotAllowed)
-	}
-}
-
 func main() {
-	lambda.Start(handler)
+	config, err := common.InitializeConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router, err := devices.Handler(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lambda.Start(router)
 }
